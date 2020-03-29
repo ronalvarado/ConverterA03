168ba193e96b470d86a0a0cacf687722c154576d
Josue Alvarado <josh28alvarado@gmail.com>
2020-03-29 00:09:38 -0600

    Revert "ya"
    This reverts commit bd5999917ef92e0763f1401c9f2251029b8b12ab.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -55,7 +55,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(253, 9);
+			this.label1.Location = new System.Drawing.Point(226, 33);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(106, 29);
 			this.label1.TabIndex = 1;
@@ -105,7 +105,7 @@
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label4.Location = new System.Drawing.Point(40, 62);
+			this.label4.Location = new System.Drawing.Point(88, 91);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(187, 23);
 			this.label4.TabIndex = 6;
